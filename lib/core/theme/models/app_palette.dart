be974c7d35f564092358bf14feea0464501b6133
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-01-03 07:03:03 -0800

    refactor: remove intermediate meta record on palettes
    This was awkward and not actually used.

--- lib/core/theme/models/app_palette.dart
+++ lib/core/theme/models/app_palette.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 
 enum AppPalette { blue, green, indigo, purple }
 
-extension AppPaletteMeta on AppPalette {
-  ({String label, Color seedColor}) get meta => switch (this) {
-    AppPalette.blue => (label: 'Blue', seedColor: Colors.blue),
-    AppPalette.green => (label: 'Green', seedColor: Colors.green),
-    AppPalette.indigo => (label: 'Indigo', seedColor: Colors.indigo),
-    AppPalette.purple => (label: 'Purple', seedColor: Colors.purple),
+extension AppPaletteProperties on AppPalette {
+  String get label => switch (this) {
+    AppPalette.blue => 'Blue',
+    AppPalette.green => 'Green',
+    AppPalette.indigo => 'Indigo',
+    AppPalette.purple => 'Purple',
   };
 
-  String get label => meta.label;
-  Color get seedColor => meta.seedColor;
+  Color get seedColor => switch (this) {
+    AppPalette.blue => Colors.blue,
+    AppPalette.green => Colors.green,
+    AppPalette.indigo => Colors.indigo,
+    AppPalette.purple => Colors.purple,
+  };
 }
