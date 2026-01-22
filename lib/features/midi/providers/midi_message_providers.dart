95ea2c9ddd8462279c766a4a8a68c65ae243561b
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-01-22 13:09:59 -0800

    Remove duplicate MIDI stream provider and tighten up the code

--- lib/features/midi/providers/midi_message_providers.dart
+++ lib/features/midi/providers/midi_message_providers.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart';
+import 'dart:typed_data';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,26 +7,22 @@ import '../services/flutter_midi_service.dart';
 
 /// Stream of raw MIDI data packets.
 final midiRawDataProvider = StreamProvider<Uint8List>((ref) {
-  ref.watch(midiControllerProvider);
-
-  final midi = ref.watch(midiCommandProvider);
-  final stream = midi.onMidiDataReceived?.map(
-    (packet) => Uint8List.fromList(packet.data),
-  );
-
-  return stream ?? const Stream<Uint8List>.empty();
+  return ref.watch(_midiRawStreamProvider);
 });
 
 /// Stream of parsed MIDI messages.
 final midiMessageProvider = StreamProvider<MidiMessage>((ref) {
+  final raw = ref.watch(_midiRawStreamProvider);
+  return raw.expand(MidiParser.parseMany);
+});
+
+final _midiRawStreamProvider = Provider<Stream<Uint8List>>((ref) {
   ref.watch(midiControllerProvider);
 
   final midi = ref.watch(midiCommandProvider);
-  final raw =
-      midi.onMidiDataReceived?.map(
+
+  return midi.onMidiDataReceived?.map(
         (packet) => Uint8List.fromList(packet.data),
       ) ??
       const Stream<Uint8List>.empty();
-
-  return raw.expand(MidiParser.parseMany);
 });
