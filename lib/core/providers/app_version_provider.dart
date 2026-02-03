a81326f254cb8db2cbe2854807770d604edcc340
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-02-02 16:59:44 -0800

    version version version version version version

--- lib/core/providers/app_version_provider.dart
+++ lib/core/providers/app_version_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-/// Package info version version string used by the app.
+/// Package info version string used by the app.
 final appVersionProvider = FutureProvider<String>((ref) async {
   final info = await PackageInfo.fromPlatform();
   return info.version;
