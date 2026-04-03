3e7af3311b91c8c04524c3d24fd0c4eb55cabc29
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-04-03 08:12:48 -0700

    Upgrade to latest Flutter stable release

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,11 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <device_manager/device_manager_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <universal_ble/universal_ble_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  DeviceManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DeviceManagerPlugin"));
   PermissionHandlerWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   UniversalBlePluginCApiRegisterWithRegistrar(
