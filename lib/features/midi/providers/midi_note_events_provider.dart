f2f159bca80558890905ab08c3525833988ba5d4
Aaron Bull Schaefer <aaron@elasticdog.com>
2026-05-11 13:47:10 -0700

    Consistently use barrel files for cross-feature imports
    There were a few cases that we've missed along the way.

--- lib/features/midi/providers/midi_note_events_provider.dart
+++ lib/features/midi/providers/midi_note_events_provider.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:whatchord/features/input/models/input_note_event.dart';
+import 'package:whatchord/features/input/input.dart';
 
 import '../models/midi_message.dart';
 import 'midi_message_providers.dart';
